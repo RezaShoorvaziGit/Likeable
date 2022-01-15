cc22ae48861d83d9d70652f089d792a96a6939d5
reza20000 <rshoorvazi@gmail.com>
2022-01-15 13:11:53 -0500

    new

---
+++ src/PackProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Reza\testpack;
+
+use Illuminate\Support\ServiceProvider;
+
+class PackProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->loadMigrationsFrom(__DIR__.'/../migrations');
+    }
+}
---
+++ src/first_model.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Reza\testpack;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class first_model extends Model
+{
+    use HasFactory;
+}
